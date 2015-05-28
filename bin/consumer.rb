a73c1000824217dfeaee0aa99f1725a40508dc8a
Peter van Hardenberg <pvh@heroku.com>
2015-05-27 23:04:45 -0700

    print a message every 100k

--- bin/consumer.rb
+++ bin/consumer.rb
@@ -5,9 +5,12 @@ host, port = JSON.parse(ENV["KAFKA_URL"])["kafka"].split(":")
 consumer = Poseidon::PartitionConsumer.new("consumer", host, port,
                                             "demoday", 0, :earliest_offset)
 
+i = 0
 loop do
   messages = consumer.fetch
   messages.each do |m|
-    puts m.value
+    m.value
+    i++
   end
+  puts "#{Time.now}: received #{i} messages" if i % 100000
 end
