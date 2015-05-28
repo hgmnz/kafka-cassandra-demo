62d95089a770e4746b2606a061f04c5076eeb882
Peter van Hardenberg <pvh@heroku.com>
2015-05-27 22:56:33 -0700

    forever

--- bin/producer.rb
+++ bin/producer.rb
@@ -4,7 +4,7 @@ require 'json'
 url = JSON.parse(ENV['KAFKA_URL'])['kafka']
 producer = Poseidon::Producer.new([url], 'producer')
 
-1000.times do |i|
-  message = Poseidon::MessageToSend.new("demoday", "Message #{i}")
+loop do
+  message = Poseidon::MessageToSend.new("demoday", "Message #{Time.now}")
   producer.send_messages([message])
 end
