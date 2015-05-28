9b8600bf0bf462c064974065953068f597d3014d
Peter van Hardenberg <pvh@heroku.com>
2015-05-27 23:07:41 -0700

    right, no ++ in ruby

--- bin/consumer.rb
+++ bin/consumer.rb
@@ -6,11 +6,12 @@ consumer = Poseidon::PartitionConsumer.new("consumer", host, port,
                                             "demoday", 0, :earliest_offset)
 
 i = 0
+
 loop do
   messages = consumer.fetch
   messages.each do |m|
     m.value
-    i++
+    i = i + 1
   end
   puts "#{Time.now}: received #{i} messages" if i % 100000
 end
